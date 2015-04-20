b3e411feee4e3f09d3dffe1e2e0c350bd55cfedc
habibah <nurhabibah@mail.ugm.ac.id>
2015-04-20 10:06:03 +0700

    Initial commit

---
+++ app/src/main/java/PageFragment.java
@@ -0,0 +1,39 @@
+/**
+ * Created by nurhabibah on 18/04/2015.
+ */
+import android.os.Bundle;
+import android.os.Bundle*
+import android.app.ActionBar;
+import android.app.FragmentTransaction;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+
+// In this case, the fragment displays simple text based on the page
+public class PageFragment extends Fragment {
+    public static final String ARG_PAGE = "ARG_PAGE";
+
+    private int mPage;
+
+    public static PageFragment newInstance(int page) {
+        Bundle args = new Bundle();
+        args.putInt(ARG_PAGE, page);
+        PageFragment fragment = new PageFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mPage = getArguments().getInt(ARG_PAGE);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_page, container, false);
+        TextView textView = (TextView) view;
+        textView.setText("Fragment #" + mPage);
+        return view;
+    }
+}
